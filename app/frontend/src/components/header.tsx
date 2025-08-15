0fe2daa4bab2b7c1e2faf0d9579a3ffa267e66e3
Yonatan Valencia <yo.valenciat@gmail.com>
2025-08-15 09:59:20 -0500

    Add tailwind

--- app/frontend/src/components/header.tsx
+++ app/frontend/src/components/header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Header = ({name: string}) => {
   return (
     <header>
-      <h1>Holaxxxx {string}</h1>
+      <h1 className="text-3xl font-bold underline">Holaxxxx {string}</h1>
     </header>
   )
 }
